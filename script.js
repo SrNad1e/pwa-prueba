07d34089112b7e7a916df167b8d0f33877320167
Camilo <camilob290421@gmail.com>
2022-10-04 08:53:54 -0500

    abcdef

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("./sw.js", {
-            scope: './'
-        })
-        .then((reg) => console.log("Registro de SW exitoso", reg))
-        .catch((err) =>
-            console.warn("Error al tratar de registrar el sw", err)
-        );
-}
+  if (
+      typeof window !== "undefined" &&
+      typeof window.navigator !== "undefined"
+  ) {
+      if ("serviceWorker" in window.navigator) {
+          window.navigator.serviceWorker
+              .register("/sw.js")
+              .then((reg) => console.log("Registro de SW exitoso", reg))
+              .catch((err) =>
+                  console.warn("Error al tratar de registrar el sw", err)
+              );
+      }
+  }
