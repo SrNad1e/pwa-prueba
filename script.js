ffcd886f21da9512acd750e2f3fed3eb4bcaee93
Camilo <camilob290421@gmail.com>
2022-10-04 08:39:35 -0500

    abcdef

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("./sw.js")
+        .register("sw.js", {
+            scope: './'
+        })
         .then((reg) => console.log("Registro de SW exitoso", reg))
         .catch((err) =>
             console.warn("Error al tratar de registrar el sw", err)
